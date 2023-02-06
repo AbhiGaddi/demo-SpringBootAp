0b454cef1e1d7d994b0f270cfdbaafef2ada072d
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-02-06 16:12:55 +0530

    feat : added publickey

--- src/main/java/com/demohcx/DemoSpringBootApplication.java
+++ src/main/java/com/demohcx/DemoSpringBootApplication.java
@@ -1,12 +1,20 @@
 package com.demohcx;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
+	private static Logger logger = LogManager.getLogger(DemoSpringBootApplication.class);
 	public static void main(String[] args) {
+        logger.info("hi info");
+		logger.debug("hey debug");
+		logger.error("hey error");
+		logger.trace("hey trace");
 		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
 
